1ed7fcf7fc52c42052c50af5b4370880cdaf7a9f
farmerlin731 <69831413+farmerlin731@users.noreply.github.com>
2025-05-26 17:20:54 +0800

    Use entity-manager to store data.

--- Chapter-03/01-cruddemo-student/src/main/java/com/luv2code/cruddemo/CruddemoApplication.java
+++ Chapter-03/01-cruddemo-student/src/main/java/com/luv2code/cruddemo/CruddemoApplication.java
@@ -1,5 +1,7 @@
 package com.luv2code.cruddemo;
 
+import com.luv2code.cruddemo.dao.StudentDAO;
+import com.luv2code.cruddemo.entity.Student;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -12,10 +14,21 @@ public class CruddemoApplication {
 		SpringApplication.run(CruddemoApplication.class, args);
 	}
 
+
 	@Bean
-	public CommandLineRunner commandLineRunner(String[] args){
+	public CommandLineRunner commandLineRunner(StudentDAO studentDAO){
 		return runner->{
-			System.out.println("Hello World from Ch3. :)");
+			createStudent(studentDAO);
 		};
+	}
+
+	private void createStudent(StudentDAO studentDAO){
+		System.out.println("Creating...");
+		Student tmpStu = new Student("Farmer","Lin","tmp@gmail.com");
+
+		System.out.println("Saving...");
+		studentDAO.save(tmpStu);
+
+		System.out.println("Finished! the saved student id :"+tmpStu.getId());
 	}
 }
