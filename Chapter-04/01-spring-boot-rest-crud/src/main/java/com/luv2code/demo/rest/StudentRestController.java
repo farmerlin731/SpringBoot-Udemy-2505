27a4332c8e73a809c7e43f5b74ac4803b03af8d3
farmerlin731 <69831413+farmerlin731@users.noreply.github.com>
2025-06-01 23:27:41 +0800

    Refactor for fake data.

--- Chapter-04/01-spring-boot-rest-crud/src/main/java/com/luv2code/demo/rest/StudentRestController.java
+++ Chapter-04/01-spring-boot-rest-crud/src/main/java/com/luv2code/demo/rest/StudentRestController.java
@@ -1,6 +1,7 @@
 package com.luv2code.demo.rest;
 
 import com.luv2code.demo.entity.Student;
+import jakarta.annotation.PostConstruct;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -11,14 +12,22 @@ import java.util.List;
 @RestController
 @RequestMapping("/api")
 public class StudentRestController {
+    private List<Student> myList;
+
+    //Set the fake data
+    @PostConstruct
+    public void loadData() {
+        myList = new ArrayList<>();
+        myList.add(new Student("Farmer", "Lin"));
+        myList.add(new Student("Bing", "Su"));
+        myList.add(new Student("Ching", "Chen"));
+        myList.add(new Student("Allen", "Wang"));
+    }
 
     //define endpoint "/student" - return a list of student
     @GetMapping("/students")
     public List<Student> getStudents() {
-        List<Student> myList = new ArrayList<>();
-        myList.add(new Student("Farmer", "Lin"));
-        myList.add(new Student("Bing", "Su"));
-        myList.add(new Student("Ching", "Chen"));
+
 
         return myList;
     }
