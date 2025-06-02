c8b855ae48abb61cae0f9954c495dad09b0a1830
farmerlin731 <69831413+farmerlin731@users.noreply.github.com>
2025-06-02 22:40:41 +0800

    Refactor: Add the service layer.

--- Chapter-04/02-rest-crud-with-employee/src/main/java/com/luv2code/springboot/cruddemo/rest/EmployeeRestController.java
+++ Chapter-04/02-rest-crud-with-employee/src/main/java/com/luv2code/springboot/cruddemo/rest/EmployeeRestController.java
@@ -1,7 +1,7 @@
 package com.luv2code.springboot.cruddemo.rest;
 
-import com.luv2code.springboot.cruddemo.dao.EmployeeDAO;
 import com.luv2code.springboot.cruddemo.entity.Employee;
+import com.luv2code.springboot.cruddemo.service.EmployeeServiceImpl;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -12,17 +12,17 @@ import java.util.List;
 @RequestMapping("/api")
 public class EmployeeRestController {
 
-    private EmployeeDAO employeeDAO;
+    private EmployeeServiceImpl employeeService;
 
     //quick and dirty : inject employee dao
-    public EmployeeRestController(EmployeeDAO theEmployeeDAO) {
-        this.employeeDAO = theEmployeeDAO;
+    public EmployeeRestController(EmployeeServiceImpl theEmployeeServiceImpl) {
+        this.employeeService = theEmployeeServiceImpl;
     }
 
     //expose"/emplyees" and return a list
     @GetMapping("/employees")
     public List<Employee> findAll() {
-        return employeeDAO.findAll();
+        return employeeService.findAll();
     }
 
 
