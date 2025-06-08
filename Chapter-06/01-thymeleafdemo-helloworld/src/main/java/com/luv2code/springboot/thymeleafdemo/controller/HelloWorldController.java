cb5d3bfc104140de5f1331ead72d08b0d1e14c6f
farmerlin731 <69831413+farmerlin731@users.noreply.github.com>
2025-06-08 15:54:59 +0800

    Set the input field.

---
+++ Chapter-06/01-thymeleafdemo-helloworld/src/main/java/com/luv2code/springboot/thymeleafdemo/controller/HelloWorldController.java
@@ -0,0 +1,21 @@
+package com.luv2code.springboot.thymeleafdemo.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HelloWorldController {
+    //Mapping to show initial form.
+    @GetMapping("/showForm")
+    public String showForm(){
+        return "helloworld-form";
+    }
+
+    //Mapping to process the HTML form.
+    @GetMapping("/processForm")
+    public String processForm(){
+        return "helloworld";
+    }
+
+
+}
