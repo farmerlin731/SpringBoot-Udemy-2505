56f85ea6dae284bb744403f9bd9c192119b15373
farmerlin731 <69831413+farmerlin731@users.noreply.github.com>
2025-06-10 18:21:28 +0800

    Show the model data binding @ student-form.

---
+++ Chapter-06/01-thymeleafdemo-helloworld/src/main/java/com/luv2code/springboot/thymeleafdemo/controller/StudentController.java
@@ -0,0 +1,32 @@
+package com.luv2code.springboot.thymeleafdemo.controller;
+
+import com.luv2code.springboot.thymeleafdemo.model.Student;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+
+@Controller
+public class StudentController {
+
+    @GetMapping("/showStudentForm")
+    public String showForm(Model theModel) {
+        //create student obj
+        Student theStu = new Student();
+
+        //add it to model
+        theModel.addAttribute("student", theStu);
+
+        return "student-form";
+    }
+
+    @PostMapping("/processStudentForm")
+    public String processStudentForm(@ModelAttribute("student") Student theStu) {
+        //log the data
+        System.out.println("In Controller - the student: " + theStu.getFirstName() + " " + theStu.getLastName());
+
+        return "student-confirmation";
+    }
+
+}
