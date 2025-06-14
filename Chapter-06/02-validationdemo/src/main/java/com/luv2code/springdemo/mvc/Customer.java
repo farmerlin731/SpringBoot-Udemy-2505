7a1f5aa0c8ab5fe32bdb07d3d58f5ec2019f8176
farmerlin731 <69831413+farmerlin731@users.noreply.github.com>
2025-06-14 18:36:15 +0800

    Add regex validation.

--- Chapter-06/02-validationdemo/src/main/java/com/luv2code/springdemo/mvc/Customer.java
+++ Chapter-06/02-validationdemo/src/main/java/com/luv2code/springdemo/mvc/Customer.java
@@ -1,9 +1,6 @@
 package com.luv2code.springdemo.mvc;
 
-import jakarta.validation.constraints.Max;
-import jakarta.validation.constraints.Min;
-import jakarta.validation.constraints.NotNull;
-import jakarta.validation.constraints.Size;
+import jakarta.validation.constraints.*;
 
 public class Customer {
     private String firstName;
@@ -15,6 +12,9 @@ public class Customer {
     @Min(value = 0, message = "Must be bigger than 0.")
     @Max(value = 10, message = "Must be smaller than 10.")
     private int freePasses;
+
+    @Pattern(regexp = "^[a-zA-Z0-9]{5}", message = "only 5 chars/digits")
+    private String postalCode;
 
     public String getFirstName() {
         return firstName;
@@ -38,5 +38,13 @@ public class Customer {
 
     public void setFreePasses(int freePasses) {
         this.freePasses = freePasses;
+    }
+
+    public String getPostalCode() {
+        return postalCode;
+    }
+
+    public void setPostalCode(String postalCode) {
+        this.postalCode = postalCode;
     }
 }
