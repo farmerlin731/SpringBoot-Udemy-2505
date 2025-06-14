63cbeddc91e21c5bb4f5722056fc0979322d2b14
farmerlin731 <69831413+farmerlin731@users.noreply.github.com>
2025-06-14 18:26:45 +0800

    Add number validation.

--- Chapter-06/02-validationdemo/src/main/java/com/luv2code/springdemo/mvc/Customer.java
+++ Chapter-06/02-validationdemo/src/main/java/com/luv2code/springdemo/mvc/Customer.java
@@ -1,14 +1,20 @@
 package com.luv2code.springdemo.mvc;
 
+import jakarta.validation.constraints.Max;
+import jakarta.validation.constraints.Min;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Size;
-                        
+
 public class Customer {
     private String firstName;
 
     @NotNull(message = "is required.")
     @Size(min = 1, message = "is required.")
     private String lastName;
+
+    @Min(value = 0, message = "Must be bigger than 0.")
+    @Max(value = 10, message = "Must be smaller than 10.")
+    private int freePasses;
 
     public String getFirstName() {
         return firstName;
@@ -24,5 +30,13 @@ public class Customer {
 
     public void setLastName(String lastName) {
         this.lastName = lastName;
+    }
+
+    public int getFreePasses() {
+        return freePasses;
+    }
+
+    public void setFreePasses(int freePasses) {
+        this.freePasses = freePasses;
     }
 }
