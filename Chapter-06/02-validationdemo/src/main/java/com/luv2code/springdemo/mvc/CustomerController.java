a8e6b158c5939767b4ac235729b2d027e73d5725
farmerlin731 <69831413+farmerlin731@users.noreply.github.com>
2025-06-14 18:11:10 +0800

    Use @InitBinder to delete white space.

--- Chapter-06/02-validationdemo/src/main/java/com/luv2code/springdemo/mvc/CustomerController.java
+++ Chapter-06/02-validationdemo/src/main/java/com/luv2code/springdemo/mvc/CustomerController.java
@@ -1,15 +1,28 @@
 package com.luv2code.springdemo.mvc;
 
 import jakarta.validation.Valid;
+import org.springframework.beans.propertyeditors.StringTrimmerEditor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.WebDataBinder;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.InitBinder;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class CustomerController {
+
+    //Add an initbinder to trim 'lastName'.
+    //Preprocess before go into controller.
+    @InitBinder
+    public void initBinder(WebDataBinder dataBinder) {
+        StringTrimmerEditor stringTrimmerEditor = new StringTrimmerEditor(true);
+        dataBinder.registerCustomEditor(String.class, stringTrimmerEditor);
+
+    }
+
     @GetMapping("/")
     public String showForm(Model theModel) {
         theModel.addAttribute("customer", new Customer());
