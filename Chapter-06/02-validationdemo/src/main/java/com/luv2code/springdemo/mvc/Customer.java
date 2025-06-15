b4901118348dc1e2e6df56596a044af0d5e4518a
farmerlin731 <69831413+farmerlin731@users.noreply.github.com>
2025-06-15 18:49:44 +0800

    Add custom validation.

--- Chapter-06/02-validationdemo/src/main/java/com/luv2code/springdemo/mvc/Customer.java
+++ Chapter-06/02-validationdemo/src/main/java/com/luv2code/springdemo/mvc/Customer.java
@@ -1,5 +1,6 @@
 package com.luv2code.springdemo.mvc;
 
+import com.luv2code.springdemo.mvc.validation.CourseCode;
 import jakarta.validation.constraints.*;
 
 public class Customer {
@@ -16,6 +17,10 @@ public class Customer {
 
     @Pattern(regexp = "^[a-zA-Z0-9]{5}", message = "only 5 chars/digits")
     private String postalCode;
+
+    //    @CourseCode
+    @CourseCode(value = "TOPS", message = "Must start with TOPS!")
+    private String courseCode;
 
     public String getFirstName() {
         return firstName;
@@ -47,5 +52,13 @@ public class Customer {
 
     public void setPostalCode(String postalCode) {
         this.postalCode = postalCode;
+    }
+
+    public String getCourseCode() {
+        return courseCode;
+    }
+
+    public void setCourseCode(String courseCode) {
+        this.courseCode = courseCode;
     }
 }
