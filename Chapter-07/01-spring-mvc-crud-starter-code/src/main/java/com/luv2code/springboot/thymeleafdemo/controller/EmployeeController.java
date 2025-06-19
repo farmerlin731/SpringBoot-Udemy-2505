a9620b114f1c9b53ce34cfbcd80fce3135b74a1d
farmerlin731 <69831413+farmerlin731@users.noreply.github.com>
2025-06-19 18:55:40 +0800

    Feature: CRUD - Update employee.

--- Chapter-07/01-spring-mvc-crud-starter-code/src/main/java/com/luv2code/springboot/thymeleafdemo/controller/EmployeeController.java
+++ Chapter-07/01-spring-mvc-crud-starter-code/src/main/java/com/luv2code/springboot/thymeleafdemo/controller/EmployeeController.java
@@ -4,10 +4,7 @@ import com.luv2code.springboot.thymeleafdemo.entity.Employee;
 import com.luv2code.springboot.thymeleafdemo.service.EmployeeService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -34,6 +31,18 @@ public class EmployeeController {
     public String showFormForAdd(Model theModel) {
         Employee theEmployee = new Employee();
         theModel.addAttribute("employee", theEmployee);
+        return ("employees/employee-form");
+    }
+
+    @GetMapping("/showFormForUpdate")
+    public String showFormForUpdate(@RequestParam("employeeId") int theId, Model theModel) {
+        //get employee
+        Employee theEmployee = employeeService.findById(theId);
+
+        //set in model
+        theModel.addAttribute("employee", theEmployee);
+
+        //send over to our form
         return ("employees/employee-form");
     }
 
