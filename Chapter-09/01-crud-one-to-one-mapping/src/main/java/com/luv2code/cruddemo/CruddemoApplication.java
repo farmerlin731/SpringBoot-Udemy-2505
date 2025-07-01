037df172b57a6f71f237cc5ffebb1a3cbde766bc
farmerlin731 <69831413+farmerlin731@users.noreply.github.com>
2025-07-01 17:28:14 +0800

    Do the CREATE function at main function.

--- Chapter-09/01-crud-one-to-one-mapping/src/main/java/com/luv2code/cruddemo/CruddemoApplication.java
+++ Chapter-09/01-crud-one-to-one-mapping/src/main/java/com/luv2code/cruddemo/CruddemoApplication.java
@@ -1,5 +1,8 @@
 package com.luv2code.cruddemo;
 
+import com.luv2code.cruddemo.dao.AppDAO;
+import com.luv2code.cruddemo.entity.Instructor;
+import com.luv2code.cruddemo.entity.InstructorDetail;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -13,9 +16,29 @@ public class CruddemoApplication {
     }
 
     @Bean
-    public CommandLineRunner commandLineRunner(String[] args) {
+    public CommandLineRunner commandLineRunner(AppDAO appDAO) {
         return runner -> {
-            System.out.println("Hello world :)");
+            createInstructor(appDAO);
         };
+    }
+
+    private void createInstructor(AppDAO appDAO) {
+        //Create instructor
+//        Instructor tmpInstructor = new Instructor("Farmer", "Lin", "hahaha@gmail.com");
+        Instructor tmpInstructor = new Instructor("George", "Chen", "hahaha@gmail.com");
+
+        //Create detail
+//        InstructorDetail tmpInstructorDetail = new InstructorDetail("http://www.luv2code.com/youtube", "skiing!!");
+        InstructorDetail tmpInstructorDetail = new InstructorDetail("http://www.luv2code.com/youtube", "dancing~~");
+
+        //Link
+        tmpInstructor.setInstructorDetail(tmpInstructorDetail);
+
+        //save
+        //note: this will save the detail object
+        //because of the cascade type
+        System.out.println("Saving... the instructor:" + tmpInstructor);
+        appDAO.save(tmpInstructor);
+        System.out.println("Finished Saving!");
     }
 }
