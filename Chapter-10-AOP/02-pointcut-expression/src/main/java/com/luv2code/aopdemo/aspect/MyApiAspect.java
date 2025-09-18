53792e7539ea396c52b5f0bbd2de25fe43362a0d
farmerlin731 <69831413+farmerlin731@users.noreply.github.com>
2025-09-18 18:59:51 +0800

    Try '@Order' annotation.

--- Chapter-10-AOP/02-pointcut-expression/src/main/java/com/luv2code/aopdemo/aspect/MyApiAspect.java
+++ Chapter-10-AOP/02-pointcut-expression/src/main/java/com/luv2code/aopdemo/aspect/MyApiAspect.java
@@ -2,10 +2,12 @@ package com.luv2code.aopdemo.aspect;
 
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Before;
+import org.springframework.core.annotation.Order;
 import org.springframework.stereotype.Component;
 
 @Aspect
 @Component
+@Order(7)
 public class MyApiAspect {
     @Before("com.luv2code.aopdemo.aspect.luvExpression.forDaoNoGetterSetter()")
     public void performApi() {
