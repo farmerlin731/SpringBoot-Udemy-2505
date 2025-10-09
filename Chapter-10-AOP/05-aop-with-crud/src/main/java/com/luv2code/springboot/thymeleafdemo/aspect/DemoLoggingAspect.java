49c2e1d67a3158a0a76f922f22fadc23b8f05427
farmerlin731 <69831413+farmerlin731@users.noreply.github.com>
2025-10-09 17:08:04 +0800

    Add @Before advice for controller/service/dao.

---
+++ Chapter-10-AOP/05-aop-with-crud/src/main/java/com/luv2code/springboot/thymeleafdemo/aspect/DemoLoggingAspect.java
@@ -0,0 +1,41 @@
+package com.luv2code.springboot.thymeleafdemo.aspect;
+
+import org.aspectj.lang.JoinPoint;
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Before;
+import org.aspectj.lang.annotation.Pointcut;
+import org.springframework.stereotype.Component;
+
+import java.util.logging.Logger;
+
+@Aspect
+@Component
+public class DemoLoggingAspect {
+
+    private Logger myLogger = Logger.getLogger(getClass().getName());
+
+    //setup pointcut declaration
+    @Pointcut("execution(* com.luv2code.springboot.thymeleafdemo.controller.*.*(..))")
+    public void forControllerPackage() {
+    }
+
+    @Pointcut("execution(* com.luv2code.springboot.thymeleafdemo.service.*.*(..))")
+    public void forServicePackage() {
+    }
+
+    @Pointcut("execution(* com.luv2code.springboot.thymeleafdemo.dao.*.*(..))")
+    public void forDaoPackage() {
+    }
+
+    @Pointcut("forControllerPackage()||forServicePackage()||forDaoPackage()")
+    public void forAppFlow() {
+
+    }
+
+    @Before("forAppFlow()")
+    public void beforeAdvice(JoinPoint theJoinPoint) {
+        String theMethods = theJoinPoint.getSignature().toShortString();
+        myLogger.info("======> In @Before Advice, the method is :" + theMethods);
+
+    }
+}
