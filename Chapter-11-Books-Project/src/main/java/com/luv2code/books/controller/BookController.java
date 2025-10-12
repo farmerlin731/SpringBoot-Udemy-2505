92ecf7214fc1c6cc0f69293f04099030235e7cac
farmerlin731 <69831413+farmerlin731@users.noreply.github.com>
2025-10-12 18:40:04 +0800

    Use filter and streams on query parameter.

--- Chapter-11-Books-Project/src/main/java/com/luv2code/books/controller/BookController.java
+++ Chapter-11-Books-Project/src/main/java/com/luv2code/books/controller/BookController.java
@@ -3,6 +3,7 @@ package com.luv2code.books.controller;
 import com.luv2code.books.entity.Book;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.ArrayList;
@@ -20,8 +21,8 @@ public class BookController {
     private void initBooks() {
         books.addAll(List.of(
                 new Book("The Silent Observer", "Alice Johnson", "Mystery"),
-                new Book("Journey to the Stars", "David Kim", "Science Fiction"),
-                new Book("The Art of Simplicity", "Marie Chen", "Philosophy"),
+                new Book("Journey to the Stars", "David Kim", "Technology"),
+                new Book("The Art of Simplicity", "Marie Chen", "Cooking"),
                 new Book("Hidden Gardens", "Liam Wong", "Lifestyle"),
                 new Book("Culinary Adventures", "Sophia Garcia", "Cooking"),
                 new Book("The Code of Tomorrow", "Ethan Brown", "Technology")
@@ -30,8 +31,12 @@ public class BookController {
 
 
     @GetMapping("/api/books")
-    public List<Book> getBooks() {
-        return books;
+    public List<Book> getBooks(@RequestParam(required = false) String category) {
+        if (category == null) return books;
+
+        return books.stream()
+                .filter(book -> book.getCategory().equalsIgnoreCase(category))
+                .toList();
     }
 
 
