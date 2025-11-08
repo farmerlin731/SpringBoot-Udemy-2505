da97c892dc9c96e9844dc97858e1706dd1943ce7
farmerlin731 <69831413+farmerlin731@users.noreply.github.com>
2025-11-08 16:03:37 +0800

    Use validation in DTO.

--- Chapter-13-Employees-Real-Project/src/main/java/com/luv2code/springboot/employees/request/EmployeeRequest.java
+++ Chapter-13-Employees-Real-Project/src/main/java/com/luv2code/springboot/employees/request/EmployeeRequest.java
@@ -1,21 +1,21 @@
 package com.luv2code.springboot.employees.request;
 
-//import jakarta.validation.constraints.Email;
-//import jakarta.validation.constraints.NotBlank;
-//import jakarta.validation.constraints.Size;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
 
 public class EmployeeRequest {
 
-    //    @NotBlank(message = "First name is mandatory")
-//    @Size(min = 2, max = 50, message = "First name must be between 2 and 50 characters")
+    @NotBlank(message = "First name is mandatory")
+    @Size(min = 2, max = 50, message = "First name must be between 2 and 50 characters")
     private String firstName;
 
-    //    @NotBlank(message = "Last name is mandatory")
-//    @Size(min = 2, max = 50, message = "Last name must be between 2 and 50 characters")
+    @NotBlank(message = "Last name is mandatory")
+    @Size(min = 2, max = 50, message = "Last name must be between 2 and 50 characters")
     private String lastName;
 
-    //    @NotBlank(message = "Email is mandatory")
-//    @Email(message = "Please provide a valid email address")
+    @NotBlank(message = "Email is mandatory")
+    @Email(message = "Please provide a valid email address")
     private String email;
 
     public EmployeeRequest(String firstName, String lastName, String email) {
